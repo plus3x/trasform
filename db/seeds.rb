d2a672324076002281bfa43bd76edfaeb803b2e2
Vladislav Petrov <electronicchest@gmail.com>
2013-07-25 03:27:47 +0400

    Add saad for catalogies

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,39 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+#puts 'Create form'
+=begin
 Form.create(
-  type_of_legal_entity: 'ООО'
-  company: 'ВелоГигант'
-  creator_name: 'Слава В.Ю.'
-  registered_adress: 'СПб, Клёвая улица 21'
-  actual_adress: 'СПб, Клёвая улица 21'
-  adress_on_english: 'SPb, Cool st. 21'
-  phone: 812.0
-  fax: 812.0
-  email: 'hrena.lusogo@el.com'
-  inn: 1545345432566.0
-  kpp: 13441325415.0
-  ogrn: 1434132434413.0
-  bank_name: 'Длинное название'
-  current_account: '31234134321'
-  correspondent_account: '32415334511'
-  bik: 32413244.0
-  bank_person: 'Личность Известная Умная'
-  type_of_certificate: 'Положительный'
-  auditors_name: 'Пётр Сергеевич Нахалов Фёдр Фёдырыч Нахалов Николай Николаевич Нахалов'
-  categories_list_of_works: '05.0	Рыболовство, рыбоводство и предоставление услуг в этих областях 05.01	Рыболовство 05.01.1	Рыболовство в открытых районах Мирового океана и внутренних морских водах'
+  type_of_legal_entity: 'ООО',
+  company: 'ВелоГигант',
+  creator_name: 'Слава В.Ю.',
+  registered_address: '123456, СПб, Клёвая улица 21',
+  actual_address: '123456, СПб, Клёвая улица 21',
+  address_on_english: '123456, SPb, Cool st. 21',
+  phone: '555-55-55',
+  fax: '8(812)123 4567',
+  email: 'hrena.lusogo@el.com',
+  inn: '1234567890',
+  kpp: '123456789',
+  ogrn: '1234567890123',
+  bank_name: 'Длинное название',
+  current_account: '12345678901234567890',
+  correspondent_account: '12345678901234567890',
+  bik: '123456789',
+  bank_person: 'Личность Известная Умная',
+  type_of_certificate: 'Положительный',
+  auditors_name: 'Пётр Сергеевич Нахалов Фёдр Фёдырыч Нахалов Николай Николаевич Нахалов',
+  categories_list_of_works: 'some'
 )
+=end
+
+Category.delete_all
+
+category_seed_data = [
+  ['1.', 'some']
+]
+
+category_seed_data.each do |path, name|
+  puts 'Create category ' + path + name
+  Category.create( path: path, name: name )
+end
